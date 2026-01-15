1520cf1f5c002a4a8d86c90b326ba215d3d81e24
xez <2817475346@qq.com>
2026-01-15 21:38:14 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 // API配置
-const API_BASE_URL = 'https://your-name-homepage.xueerze-ef3.workers.dev';
+const API_BASE_URL = 'https://home.xez.us.ci';
 
 // GitHub用户名将从API获取
 let GITHUB_USERNAME = 'IonRh'; // 默认值，将被API数据覆盖
